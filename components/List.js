c8bf8705f0552ec732bb9ecae1adee744f60a9b6
Tiia Toivonen <tiiatoiv@metropolia.fi>
2020-01-15 11:33:25 +0200

    Fetch from url and API hooks

--- components/List.js
+++ components/List.js
@@ -1,21 +1,26 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, {useContext} from 'react';
 import {
   FlatList,
 } from 'react-native';
 import ListItem from './ListItem';
 import PropTypes from 'prop-types';
+import {MediaContext} from '../contexts/MediaContext';
+import {useFetch} from '../hooks/APIHooks';
 
 const List = (props) => {
-  console.log(props);
+  const [media, setMedia] = useContext(MediaContext);
+  const [data, loading] = useFetch('https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json');
+  console.log('List', data, loading);
+  setMedia(data);
   return (
     <FlatList
-      data={props.mediaArray}
+      data={media}
+      keyExtractor={(item, index) => index.toString()}
       renderItem={({item}) => <ListItem singleMedia={item} />}
     />
   );
 };
-
 List.propTypes = {
   mediaArray: PropTypes.array,
 };
